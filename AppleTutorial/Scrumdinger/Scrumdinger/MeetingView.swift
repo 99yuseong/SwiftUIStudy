2b276be19964250fd33dbebb94e294ea7cf5f600
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-03-10 17:23:35 +0900

    Step 1: Getting started with Scrumdinger

---
+++ AppleTutorial/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by 남유성 on 3/10/24.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .leading) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .background(.red)
+        .padding()
+        .background(.yellow, ignoresSafeAreaEdges: [])
+    }
+}
+
+#Preview {
+    MeetingView()
+}
