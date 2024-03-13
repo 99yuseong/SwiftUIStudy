f44355d783896e4ed9e6131d719e50ff28c41f0a
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-03-13 20:48:57 +0900

    Step 8: Managing state and life cycle

---
+++ AppleTutorial/Scrumdinger/Scrumdinger/View/Meeting/MeetingHeaderView.swift
@@ -0,0 +1,56 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by 남유성 on 3/13/24.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    let theme: Theme
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.tophalf.fill")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
+                        .labelStyle(.trailingIcon)
+                }
+            }
+        }
+        .padding([.top, .horizontal])
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 400, height: 60)) {
+    MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum)
+        .previewLayout(.sizeThatFits)
+}
